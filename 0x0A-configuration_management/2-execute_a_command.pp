1b1b0df82cbe0b19bcb855d5a42b00a4941d87b9
Anna <annastacia78@gmail.com>
2023-02-04 13:06:06 -0800

    config mgt tasks

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+#create a manifest that kills a process named killmenow
+#Must use the exec Puppet resource
+#Must use pkill
+
+exec { 'kill_process':
+  command     => 'pkill killmenow',
+  path        => '/usr/bin:/usr/sbin:/bin',
+  refreshonly => true,
+}
